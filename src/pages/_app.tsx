733f16ebe1e34390434a4e59136a5f2670e828e7
Michael Penick <penick@gmail.com>
2023-06-30 20:23:29 -0400

    Add auth to amplify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { Amplify } from 'aws-amplify';
+import "../styles/globals.css";
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from '../aws-exports';
+Amplify.configure(awsExports);
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
