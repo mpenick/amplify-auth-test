024c390c5c46046e8f170d9d280d39e3c2169279
Michael Penick <penick@gmail.com>
2023-07-01 09:57:38 -0400

    Add ability to change username

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,36 @@
-import { withAuthenticator } from "@aws-amplify/ui-react"
+import { Button, withAuthenticator } from "@aws-amplify/ui-react"
+
+import { Auth } from "aws-amplify"
+import { useId, useState } from "react"
 
 export default withAuthenticator(function Home({signOut, user}) {
+  const [input, setInput] = useState('')
+
+  function handleUserChange() {
+    console.log(input)
+
+    const setUsername = async() => {
+      let user = await Auth.currentAuthenticatedUser()
+      const result = await Auth.updateUserAttributes(user, {"preferred_username": input})
+      console.log(result)
+    }
+
+    setUsername().catch(console.error)
+  }
+
+  const id = useId()
+
   return (
     <div style={{ padding: 50 }}>
       <h1>Logged in as {user?.attributes?.preferred_username}.</h1>
       <div>
-        <button onClick={signOut}>Sign out</button>
+        <input id={id} value={input} onInput={e => setInput(e.target.value)} type="text" />
+      </div>
+      <div>
+        <Button onClick={handleUserChange}>Change username</Button>
+      </div>
+      <div>
+        <Button onClick={signOut}>Sign out</Button>
       </div>
     </div>
   )
