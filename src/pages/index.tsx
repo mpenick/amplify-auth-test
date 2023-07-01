07b0d49d43052064a28cf90acb11f046fee7fc52
Michael Penick <penick@gmail.com>
2023-06-30 21:18:19 -0400

    Change to show preferred username

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { withAuthenticator } from "@aws-amplify/ui-react"
 export default withAuthenticator(function Home({signOut, user}) {
   return (
     <div style={{ padding: 50 }}>
-      <h1>Logged in as {user?.username}.</h1>
+      <h1>Logged in as {user?.attributes?.preferred_username}.</h1>
       <div>
         <button onClick={signOut}>Sign out</button>
       </div>
